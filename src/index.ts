881a596becbc2f16a831eb4db458cdb3ffe1e8dd
Kevin Souza <kevinwillyansouza@gmail.com>
2023-10-24 10:17:53 -0400

    feat: arquivo de test, instalação do axios e rota post provisória

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
     res.send("Olá, mundo!");
 });
 
+app.post("/filmes", (req, res) => {
+    return res.status(201).json({
+        titulo: "Vingadores",
+        descricao: "Filme dos Vingadores",
+        foto: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/06/Homura-akemi-Cropped.jpg",
+    });
+});
+
 app.listen(port, () => {
     console.log(
         `Servidor iniciado na porta: ${port} - http://localhost:${port}`
---
+++ test/index.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from "vitest";
+import axios from "axios";
+
+describe("Primeiro teste da aplicação", () => {
+    it("should pass", () => {
+        expect(true).toBe(true);
+    });
+});
+
+describe("Cadastro Filme", () => {
+    it("Deve cadastrar um filme com sucesso", async () => {
+        const filme = {
+            titulo: "Vingadores",
+            descricao: "Filme dos Vingadores",
+            foto: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/06/Homura-akemi-Cropped.jpg",
+        };
+        const resposta = await axios.post(
+            "http://localhost:3000/filmes",
+            filme
+        );
+
+        expect(resposta.status).toBe(201);
+        expect(resposta.data).toEqual(filme);
+    });
+});
